6ac35e73a9bdf5cfa678c250368b90070f074ed8
Rachael Baumann <baumann.rachael@gmail.com>
2018-10-11 09:42:52 -0400

    more edits to friends

--- js/friends.js
+++ js/friends.js
@@ -1,26 +1,26 @@
 var friends = [
-    { name: "Monty", favcolor: "blue", favnumber: 42 },
-    { name: "Joel", favcolor: "green", favnumber: 7576 },
-    { name: "Andrew", favcolor: "purple", favnumber: 13 },
-    { name: "Strang", favcolor: "purple", favnumber: 13 },
-    { name: "Steven", favcolor: "orange", favnumber: 10 },
-    { name: "Lance", favcolor:"blue", favnumber: 21 },
-    { name: "Angie", favcolor: "pink", favnumber: 7 },
-    { name: "Mike", favcolor: "green", favnumber: 23 },
-    { name: "Nick", favcolor: "blue", favnumber: 6 },
-    { name: "Emilie", favcolor: "sage", favnumber: 13 },
-    { name: "Robert", favcolor: "purple", favnumber: 31 },
-    { name: "Phil", favcolor: "green", favnumber: 13 },
-    { name: "Sheila", favcolor: "purple", favnumber: 17 },
-    { name: "Rachael", favcolor: "black", favnumber: 666 }
+    { name: "Monty",   favcolor: "blue",   favnumber: 42   },
+    { name: "Joel",    favcolor: "green",  favnumber: 7576 },
+    { name: "Andrew",  favcolor: "purple", favnumber: 13   },
+    { name: "Strang",  favcolor: "purple", favnumber: 13   },
+    { name: "Steven",  favcolor: "orange", favnumber: 10   },
+    { name: "Lance",   favcolor: "blue",   favnumber: 21   },
+    { name: "Angie",   favcolor: "pink",   favnumber: 7    },
+    { name: "Mike",    favcolor: "green",  favnumber: 23   },
+    { name: "Nick",    favcolor: "blue",   favnumber: 6    },
+    { name: "Emilie",  favcolor: "sage",   favnumber: 13   },
+    { name: "Robert",  favcolor: "purple", favnumber: 31   },
+    { name: "Phil",    favcolor: "green",  favnumber: 13   },
+    { name: "Sheila",  favcolor: "purple", favnumber: 17   },
+    { name: "Rachael", favcolor: "black",  favnumber: 666  }
 ];
 
 $().ready(function() {
 
-    var tbody = $("#tb");
-    tbody.empty();
+    var tbody = $("#tb"); // gets a reference to the tbody tag so we can add the new lines into HTML dynamically
+    tbody.empty(); // clears any HTML tags within tbody
 
-    for(var friend of friends) {
+    for(var friend of friends) { // just like an enhanced for loop
         var row = "<tr>";
         row += "<td>" + friend.name          + "</td>";
         row += "<td>" + friend.favcolor      + "</td>";
